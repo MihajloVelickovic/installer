aa94858b4b8cec9e19145359ff99f19a175eade9
MihajloVelickovic <75692622+MihajloVelickovic@users.noreply.github.com>
2022-08-03 21:25:08 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo pacman -S --noconfirm rofi nitrogen dunst zsh sxhkd bspwm polybar curl lightdm xorg xorg-xinit lightshot xfce4-terminal firefox
-chsh -s $(which zsh)
+sudo pacman -S --noconfirm rofi nitrogen dunst zsh sxhkd bspwm polybar curl lightdm xf86-video-qxl xorg xorg-xinit flameshot xfce4-terminal firefox
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd Downloads
 git clone https://aur.archlinux.org/yay.git
@@ -9,11 +8,19 @@ makepkg -si
 cd ..
 yay -S --noconfirm picom-ibhagwan-git lightdm-webkit2-greeter lightdm-webkit2-theme-reactive betterlockscreen
 cd ~/.config
-git clone https://github.com/MihajloVelickovic/dotFiles .
+git clone https://github.com/MihajloVelickovic/dotFiles 
+cd dotFiles
+mv bspwm ..
+mv sxhkd..
+mv picom ..
+mv polybar ..
+mv dunst ..
+mv rofi ..
 rm .gitattributes
 mv .xinitrc ~
 mv .zshrc ~
 sudo mv lightdm.conf /etc/lightdm/
+sudo mv lightdm-webkit2-greeter.conf
 cd ..
 ln -sf .xinitrc .xsession
 
