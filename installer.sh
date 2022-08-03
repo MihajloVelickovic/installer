226da00464ac59add47d76fe34403e85829ca98d
MihajloVelickovic <75692622+MihajloVelickovic@users.noreply.github.com>
2022-08-03 16:02:08 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S rofi nitrogen dunst zsh sxhkd bspwm polybar curl lightdm xorg xorg-xinit lightshot xfce4-terminal firefox
+sudo pacman -S --noconfirm rofi nitrogen dunst zsh sxhkd bspwm polybar curl lightdm xorg xorg-xinit lightshot xfce4-terminal firefox
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd Downloads
@@ -7,7 +7,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
-yay -S picom-ibhagwan-git lightdm-webkit2-greeter lightdm-webkit2-theme-reactive betterlockscreen
+yay -S --noconfirm picom-ibhagwan-git lightdm-webkit2-greeter lightdm-webkit2-theme-reactive betterlockscreen
 cd ~/.config
 git clone https://github.com/MihajloVelickovic/dotFiles .
 rm .gitattributes
