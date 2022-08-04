78607cd7046bc85bdbbf677eebace6a1f62578c5
MihajloVelickovic <75692622+MihajloVelickovic@users.noreply.github.com>
2022-08-04 03:36:36 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 rm .gitattributes
-sudo pacman -S --noconfirm ranger pulseaudio alsa-utils pacman-contrib rofi nitrogen dunst zsh sxhkd bspwm polybar curl lightdm xf86-video-qxl xorg xorg-xinit flameshot xfce4-terminal firefox
+cd ~
+sudo pacman -S --noconfirm ranger pulseaudio alsa-utils spotify discord pacman-contrib rofi nitrogen dunst zsh sxhkd bspwm polybar curl lightdm xf86-video-qxl xorg xorg-xinit flameshot xfce4-terminal firefox
 cd Downloads
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
-yay -S --noconfirm picom-ibhagwan-git lightdm-webkit2-greeter lightdm-webkit2-theme-reactive betterlockscreen nerd-fonts-complete noto-fonts-cjk ttf-ms-win11-auto
+yay -S --noconfirm picom-ibhagwan-git spotify-adblock lightdm-webkit2-greeter lightdm-webkit2-theme-reactive betterlockscreen nerd-fonts-complete noto-fonts-cjk ttf-ms-win11-auto
 cd ~/.config
 git clone https://github.com/MihajloVelickovic/dotFiles 
 cd dotFiles
-mv bspwm ..
-mv sxhkd ..
-mv picom ..
-mv polybar ..
-mv dunst ..
-mv rofi ..
-mv xfce4 ..
+mv bspwm sxhkd picom polybar dunst rofi xfce4 ..
 rm .gitattributes
-mv .xinitrc ~
-mv .zshrc ~
-sudo mv lightdm.conf /etc/lightdm/
-sudo mv lightdm-webkit2-greeter.conf /etc/lightdm
+mv .xinitrc .zshrc ~
+sudo mv lightdm.conf lightdm-webkit2-greeter.conf /etc/lightdm/
 sudo systemctl enable lightdm
 cd ..
 rm -rf dotFiles
@@ -32,5 +25,3 @@ rm -rf .cache
 yay -Sc --noconfirm
 sudo pacman -Rns --noconfirm $(pacman -Qtdq)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-chsh -s $(which zsh)
